cda6a75449b2e6960b81b4066a543c907f4a18bf
Theo <usertb0.pro@gmail.com>
2022-09-20 17:22:46 +0200

    Post list

---
+++ src/views/PostListView.vue
@@ -0,0 +1,20 @@
+<template>
+    <HeaderLayout />
+    <ListLayout />
+    <FooterLayout />
+</template>
+
+<script>
+    import HeaderLayout from '@/components/template/HeaderLayout.vue'
+    import FooterLayout from '@/components/template/FooterLayout.vue'
+    import ListLayout from '@/components/post/ListLayout.vue'
+
+    export default {
+        components:
+        {
+            HeaderLayout,
+            FooterLayout,
+            ListLayout
+        }
+    }
+</script>

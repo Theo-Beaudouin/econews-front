03c8e22eda97e59da1335059ace0ff69e5d04ad7
Theo <usertb0.pro@gmail.com>
2022-09-22 16:09:25 +0200

    Burger menu

--- src/components/template/HeaderLayout.vue
+++ src/components/template/HeaderLayout.vue
@@ -3,19 +3,35 @@
         <div class="header-ctn">
             <i class="fa-solid fa-magnifying-glass"></i>
             <img class="header-logo" src="@/assets/logo.png" alt="Circular econews logo depicting a golden tree with a green background">
-            <p class="header-menu">
-                Username
-                <i class="fa-solid fa-circle-arrow-up"></i>
-            </p>
+            <div class="header-menu">
+                <p class="menu-title">
+                    Username
+                    <i class="fa-solid fa-circle-arrow-right"></i>
+                </p>
+            </div>
+        </div>
+
+        <div class="menu-ctn">
+            <div class="menu-options-list">
+                <router-link :to="{ name: userAccount }" class="options-item">
+                    Account
+                </router-link>
+                <router-link :to="{ name: category }" class="options-item">
+                    Categories
+                </router-link>
+                <router-link :to="{ name: createPost }" class="options-item">
+                    Create a post
+                </router-link>
+                <router-link :to="{ name: home }" class="options-item logout">
+                    Log out
+                </router-link>
+                <i class="fa-regular fa-circle-xmark"></i>
+            </div>
         </div>
     </header>
 </template>
 
-<script>
-export default {
-
-}
-</script>
+<script></script>
 
 <style scoped>
     header
@@ -44,10 +60,77 @@ export default {
         margin: auto;
     }
 
-    .header-menu
+    .menu-title
     {
         font-weight: 500;
         font-size: 15px;
+        animation: rotate-off 0.5s ease-in;
+    }
+
+    .menu-title:hover
+    {
+        color: #ffde59;
+        text-decoration: underline;
+        cursor: pointer;
+    }
+
+    /*
+        .menu-options-list is here to counter the background-color of the <header>.
+        Because I do not want a css attribute of "display: absolute",
+        the menu is part of the header, his 100% width and green background color.
+        This is why the only reason this class exist
+        is for having a white background on the sides of the menu.
+    */
+
+    .menu-ctn
+    {
+        background-color: #fff;
+    }
+
+    .menu-options-list
+    {
+        border-top: solid 1px #ffde59;
+        background-color: #043419;
+        width: 1024px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 20px;
+        margin: auto;
+        padding: 10px;
+        border-radius: 0 0 10px 10px;
+    }
+
+    .options-item
+    {
+        color: #fff;
+        font-weight: 500;
+        font-size: 17px;
+        text-decoration: none;
+    }
+
+    .options-item:hover
+    {
+        color: #ffde59;
+        text-decoration: underline;
+    }
+
+    .logout:hover
+    {
+        color: #fe4646;
+    }
+
+    .fa-circle-xmark
+    {
+        color: #ffde59;
+        font-size: 25px;
+    }
+
+    .fa-circle-xmark:hover
+    {
+        color: #fe4646;
+        font-size: 25px;
+        cursor: pointer;
     }
 
     @media (min-width: 320px) and (max-width: 767px)

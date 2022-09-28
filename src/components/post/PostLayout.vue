ea79f572b1ff95cd25ee2724174d3560ea2f1a7f
Theo <usertb0.pro@gmail.com>
2022-09-28 14:28:54 +0200

    Props in PostLayout component

--- src/components/post/PostLayout.vue
+++ src/components/post/PostLayout.vue
@@ -37,6 +37,25 @@
     </main>
 </template>
 
+<script>
+    export default {
+        /// Props are a way to make a Vue App more modular.
+        /// They are data that can be passed from a child compoment to his parent.
+        /// In my case, I want to use the PostLayout component throughout the App,
+        /// if I were to not use props and put all the logic to insert the data received from the API,
+        /// I would have to create a new component that would do a similar task as this one.
+        /// Props allows me to have a lighter App, helps to sustain it and makes developmnent easier and faster.
+        props: {
+            title: String,
+            imgSrc: String,
+            imgAlt: String,
+            excerpt: String,
+            date: String,
+            author: String
+        }
+    }
+</script>
+
 <style scoped>
 
     .main-title
